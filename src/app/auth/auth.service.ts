890ba60062d0eb819a834edd37cc30f3277a8be2
Samuel Alves <samuel241296@hotmail.com>
2020-09-03 21:10:40 -0300

    Section-23 - Deploy to firebase and environment variables

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { catchError, tap } from 'rxjs/operators'
 import { throwError, BehaviorSubject } from 'rxjs'
 import { User } from './user.model'
 import { Router } from '@angular/router'
+import { environment } from '../../environments/environment'
 
 export interface AuthResponseData {
   kind: string
@@ -26,7 +27,7 @@ export class AuthService{
 
   signup(email: string, password: string){
     return this.http.post<AuthResponseData>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyALyuDW0vFmGOyP5GdMgtdOJViYxHDeKEk`,
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.firebaseAPIKey}`,
       { email, password, returnSecureToken: true }
     ).pipe(
         catchError(this.handleError),
@@ -38,7 +39,7 @@ export class AuthService{
 
   login(email: string, password: string){
     return this.http.post<AuthResponseData>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyALyuDW0vFmGOyP5GdMgtdOJViYxHDeKEk`,
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.firebaseAPIKey}`,
       { email,password, returnSecureToken: true }
     ).pipe(
         catchError(this.handleError),
