963880c11a4f4f974f8038960efb0437b401f32f
Samuel Alves <samuel241296@hotmail.com>
2020-09-06 17:21:55 -0300

    Section24 - Ngrx - Redux DevTools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { StoreModule } from '@ngrx/store'
 import { EffectsModule } from '@ngrx/effects'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+import { StoreRouterConnectingModule } from '@ngrx/router-store'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,6 +13,7 @@ import { CoreModule } from './core.module'
 import * as fromApp from './store/app.reducer'
 import { SharedModule } from './shared/shared.module';
 import { AuthEffects } from './auth/store/auth.effects'
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -21,6 +24,8 @@ import { AuthEffects } from './auth/store/auth.effects'
     BrowserModule,
     HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
     EffectsModule.forRoot([AuthEffects]),
     AppRoutingModule,
     SharedModule,
